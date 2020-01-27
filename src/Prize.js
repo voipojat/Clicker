096070531f922fd37719eb72b5868afd96c6374d
tuomiaey <anton.e.tuominen@student.jyu.fi>
2020-01-27 15:31:45 +0200

    updating

--- src/Prize.js
+++ src/Prize.js
@@ -2,7 +2,7 @@
 function prize(gamePoints, userPoints, changeReward, changeStatus) {
     if ((gamePoints + 1) % 500 === 0 && gamePoints !== 0) {
         changeReward(250);
-        localStorage.setItem("userPoints", userPoints + 250)
+        localStorage.setItem("userPoints", userPoints + 249)
         changeStatus(true);
         setTimeout(() => {
             changeStatus(false)
@@ -11,7 +11,7 @@ function prize(gamePoints, userPoints, changeReward, changeStatus) {
     }
     if ((gamePoints + 1) % 100 === 0 && gamePoints !== 0) {
         changeReward(40);
-        localStorage.setItem("userPoints", userPoints + 40)
+        localStorage.setItem("userPoints", userPoints + 39)
         changeStatus(true);
         setTimeout(() => {
             changeStatus(false)
@@ -20,7 +20,7 @@ function prize(gamePoints, userPoints, changeReward, changeStatus) {
     }
     if ((gamePoints + 1) % 10 === 0 && gamePoints !== 0) {
         changeReward(5);
-        localStorage.setItem("userPoints", userPoints + 5)
+        localStorage.setItem("userPoints", userPoints + 4)
         changeStatus(true);
         setTimeout(() => {
             changeStatus(false)
